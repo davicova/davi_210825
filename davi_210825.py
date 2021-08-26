cb9d671b1eb7b6885f6e3a946321008865c26582
davicova <86981409+davicova@users.noreply.github.com>
2021-08-25 21:06:17 -0300

    Add files via upload

---
+++ davi_210825.py
@@ -0,0 +1,13 @@
+lista1=["brasil","belgica","bolivia","brunei","congo","México"]
+print("Question 2 ************************")
+print(lista1)
+print("Question 3 ************************")
+print(lista1[:5:2])
+print("Question 4 ************************")
+for item in lista1:
+    print(lista1[1:4])
+print(lista1)
+print("Question 5 ************************")
+lst=["one", "TWO", "THREE", "four", "five", "SIX", "seven"]
+print("Question 6 ************************")
+for item if 
